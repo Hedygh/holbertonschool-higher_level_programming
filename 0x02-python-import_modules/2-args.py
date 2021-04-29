8fd0f0fc718857a2192e611de910f75de14f1704
hedygh <dyouahes@gmail.com>
2021-04-29 20:23:45 +0200

    add : for first line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print(len(sys.argv)-1, "argument")
+        print(len(sys.argv)-1, "argument:")
     else:
-        print(len(sys.argv)-1, "arguments")
+        print(len(sys.argv)-1, "arguments:")
     i = 1
     while i < len(sys.argv):
         print("{:d}: {:s}".format(i, sys.argv[i]))
