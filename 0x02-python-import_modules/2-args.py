bcfd35500deaa38a2d59a213acdd418f4014e3d4
hedygh <dyouahes@gmail.com>
2021-04-29 20:22:28 +0200

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(len(sys.argv)-1, "argument")
+    else:
+        print(len(sys.argv)-1, "arguments")
+    i = 1
+    while i < len(sys.argv):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
+        i = i + 1
