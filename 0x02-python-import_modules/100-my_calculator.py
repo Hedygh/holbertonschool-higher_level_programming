b682630780c5a1a98730317086d384ff0f4be4fc
hedygh <dyouahes@gmail.com>
2021-04-29 21:52:07 +0200

    import syntax

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
     exit()
-if (len(sys.argv)) != 4:
+if (len(argv)) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
-a = int(sys.argv[1])
-b = int(sys.argv[3])
+a = int(argv[1])
+b = int(argv[3])
 operators = ["+", "-", "*", "/"]
 functions = [add, sub, mul, div]
-if sys.argv[2] not in operators:
+if argv[2] not in operators:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 i = 0
 while i < 4:
-    if sys.argv[2] == operators[i]:
         print("{:d} {:s} {:d} = {:d}".format
               (a, operators[i], b, functions[i](a, b)))
         break
-    i = i + 1
+        i = i + 1
