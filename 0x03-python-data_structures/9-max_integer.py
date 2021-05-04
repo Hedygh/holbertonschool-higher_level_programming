40dcfe947008189efc660e00c0e472ffa6dcccc9
hedygh <dyouahes@gmail.com>
2021-05-04 21:39:48 +0200

    assignement on first value should be 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    res = 0
+    res = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > res:
             res = my_list[i]
