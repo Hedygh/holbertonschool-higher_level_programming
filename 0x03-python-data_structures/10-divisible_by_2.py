f8ba52380d9580d3a76b786815d4680b041eb1ef
hedygh <dyouahes@gmail.com>
2021-05-04 21:40:55 +0200

    missings tasks

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    _list = list(my_list)
+    for i in my_list:
+        if my_list[i] % 2 == 0:
+            _list[i] = True
+        else:
+            _list[i] = False
+    return _list
