b8f2bd1e4e84fd76354c6841b8eb67905cb598e5
hedygh <dyouahes@gmail.com>
2021-05-06 14:43:08 +0200

    key get

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     else:
-        return (max(a_dictionary))
+        return (max(a_dictionary, key=a_dictionary.get))
