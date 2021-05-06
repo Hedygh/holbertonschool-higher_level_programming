80b38ab1ce70816e093f824572ec746fdfdaf22b
hedygh <dyouahes@gmail.com>
2021-05-06 16:06:39 +0200

    forget print

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
     tmp = []
     for k, v in a_dictionary.items():
         if v == value:
             tmp.append(k)
     for k in tmp:
         del(a_dictionary[k])
-    print(tmp)
     return a_dictionary
