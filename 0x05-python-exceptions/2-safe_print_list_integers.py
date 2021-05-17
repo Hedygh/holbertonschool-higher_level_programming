2fdf452e375a8afee91c083c4051d349e76524f8
hedygh <dyouahes@gmail.com>
2021-05-17 19:11:22 +0200

    only one except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             index += 1
-        except TypeError:
-            continue
-        except ValueError:
+        except (ValueError, TypeError):
             continue
     print('')
     return index
