79f41d6daed988e09cc9afd96b67bb565a1c02be
hedygh <dyouahes@gmail.com>
2021-05-17 21:12:17 +0200

    task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        print("Exception: {}".format(value), file=sys.stderr)
+        return False
