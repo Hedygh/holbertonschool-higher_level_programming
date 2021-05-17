56cf08afc74b6bfcced162c538703dbab2a2f414
hedygh <dyouahes@gmail.com>
2021-05-17 21:28:32 +0200

    no return fct but var

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         res = fct(*args)
-        return res
     except Exception as e:
         print("Exceptions: {}".format(e), file=sys.stderr)
         return None
+    return res
