0ff2f23409de9d7d90131bfa23777b2ce4bb7b1a
hedygh <dyouahes@gmail.com>
2021-05-18 21:34:21 +0200

    idk

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,11 +50,11 @@ class Square:
             print()
             return
 
-        for i in range(self.__position[1]):
+        for i in range(0, self.__position[1]):
             print()
-        for i in range(self.__size):
-            for n in range(self.__position[0]):
+        for i in range(0, self.__size):
+            for n in range(0, self.__position[0]):
                 print(" ", end='')
-            for j in range(self.__size):
+            for j in range(0, self.__size):
                 print("#", end='')
             print()
