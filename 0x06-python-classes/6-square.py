a0da1be844eb3700dba8d045ae88d7e2bc0a719b
hedygh <dyouahes@gmail.com>
2021-05-18 21:23:02 +0200

    remove doc

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,13 +5,7 @@
 
 
 class Square:
-    """ Define Square class.
-
-
-    Attributes:
-        size: size
-        position: position
-    """
+    """ Define Square class."""
 
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
@@ -25,7 +19,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
