7fafa45672920bd598f6ad0026c0d044964576f5
hedygh <dyouahes@gmail.com>
2021-05-19 03:14:30 +0200

    order if statements MATTER

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,10 +35,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                not all(n >= 0 for n in value) or
-                len(value) != 2 or
-                not isinstance(n, int) for n in value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
