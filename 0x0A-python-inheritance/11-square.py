09a9e1ecb38826b81025415de035c0dd622e1a51
hedygh <dyouahes@gmail.com>
2021-06-01 22:21:03 +0200

    print rec instead of square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,5 +7,7 @@ class Square(Rectangle):
     """ defines Square sub class """
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
