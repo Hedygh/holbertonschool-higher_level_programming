fda8acc85f09384ccfc60b82cc34a717af96bb1d
hedygh <dyouahes@gmail.com>
2021-06-03 22:43:40 +0200

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """ save to json module """
     with open(filename, 'w') as f:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, f)
     f.closed
