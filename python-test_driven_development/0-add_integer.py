a637cd532d396b6a570d96392de4745fd92e8e90
Hedygh <dyouahes@gmail.com>
2026-01-22 21:05:31 +0100

    add bool case

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Function that add two integers.
-
-
-
-"""
+"""This module provides a function that adds two integers."""
 
 
 def add_integer(a, b=98):
-    """
-    :param a: int to add :param b: int to add
+    """Return the addition of a and b as integers.
+
+    a and b must be integers or floats. Floats are cast to integers before
+    performing the addition.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    if isinstance(a, bool) or not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if isinstance(b, bool) or not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
