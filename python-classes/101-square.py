d56eb90335a09058e4f3537b8a966b36532f375e
Hedygh <dyouahes@gmail.com>
2026-01-28 23:36:41 +0100

    101

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -1,65 +1,73 @@
 #!/usr/bin/python3
-"""Class Square that defines a square."""
+""" Defines class Square with size attribute.
+    Raise error for type and value.
+    Calculate and return the square area"""
 
 
 class Square:
-    """Square class."""
+    """ Define Square class."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a square with optional size and position."""
+        """ Initialize Square.
+
+        Args:
+            size (int): Size of square
+            position (int, int): position of square
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get the current size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """Get the current position."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Set the position with validation."""
         if (not isinstance(value, tuple) or len(value) != 2 or
-                not isinstance(value[0], int)
-                or not isinstance(value[1], int) or
-                value[0] < 0 or value[1] < 0):
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return the current square area."""
-        return self.__size * self.__size
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square using # and position."""
-        print(self, end="")
+        if self.__size <= 0:
+            print()
+            return
+
+        for i in range(0, self.__position[1]):
+            print()
+        for i in range(0, self.__size):
+            for n in range(0, self.__position[0]):
+                print(" ", end='')
+            for j in range(0, self.__size):
+                print("#", end='')
+            print()
 
     def __str__(self):
-        """Return the square representation as a string."""
-        if self.__size == 0:
-            return "\n"
-
-        lines = []
-
-        # vertical offset (blank lines)
-        for _ in range(self.__position[1]):
-            lines.append("")
-
-        # square lines with horizontal offset (spaces)
-        for _ in range(self.__size):
-            lines.append(" " * self.__position[0] + "#" * self.__size)
-
-        return "\n".join(lines)
+        if self.__size <= 0:
+            return('')
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end='')
+            for n in range(self.__size):
+                print("#", end='')
+            if i != self.__size - 1:
+                print()
+        return('')
