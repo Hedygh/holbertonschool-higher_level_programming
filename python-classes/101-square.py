3095f859d4af3d6c3be1d18da0b98c000354575e
Hedygh <dyouahes@gmail.com>
2026-01-28 23:37:57 +0100

    101

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -60,7 +60,7 @@ class Square:
 
     def __str__(self):
         if self.__size <= 0:
-            return('')
+            return ('')
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
@@ -70,4 +70,4 @@ class Square:
                 print("#", end='')
             if i != self.__size - 1:
                 print()
-        return('')
+        return ('')
