3022c53d915ced0d4b6110cff5be6c814c6f81d2
Hedygh <dyouahes@gmail.com>
2026-02-04 18:45:01 +0100

    add docst

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ Class Rectangle inherite BaseGeometry """
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
