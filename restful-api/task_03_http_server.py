e2667b3de14b300415390bb4ff9d2a64e827bae8
Hedygh <dyouahes@gmail.com>
2026-02-19 21:06:59 +0100

    task 03

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" simple http server module """
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class APIhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            body = "Hello; this is a simple API!".encode("utf-8")
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(body)
+        elif self.path == "/info":
+            data = {"version": "1.0", "description": "A simple API built with http.server"}
+            body = json.dumps(data).encode("utf-8")
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(body)
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            body = json.dumps(data).encode("utf-8")
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(body)
+        elif self.path == "/status":
+            body = "OK".encode("utf-8")
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(body)
+        else:
+            body = "Endpoint not found".encode("utf-8")
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(body)
+
+if __name__ == "__main__":
+    httpd = HTTPServer(("", 8000), APIhandler)
+    httpd.serve_forever()
