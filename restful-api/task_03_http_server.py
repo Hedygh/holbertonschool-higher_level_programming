59aa006ff2e670eef6396a515c02e49f4ce9f636
Hedygh <dyouahes@gmail.com>
2026-02-19 21:21:20 +0100

    change check order

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-""" simple http server module """
+"""Simple API built with http.server."""
 
-
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler
 import json
 
 
-class APIhandler(BaseHTTPRequestHandler):
-    """ mini API that send res_ponses"""
+class APIHandler(BaseHTTPRequestHandler):
+    """Handle API endpoints for a simple HTTP server."""
+
     def do_GET(self):
-        """ methods that defines responses for given path """
+        """Handle GET requests."""
         if self.path == "/":
             body = "Hello, this is a simple API!".encode("utf-8")
             self.send_response(200)
             self.send_header("Content-Type", "text/plain; charset=utf-8")
             self.end_headers()
             self.wfile.write(body)
-        elif self.path == "/info":
-            data = {
-                "version": "1.0",
-                "description": "A simple API built with http.server"
-                }
-            body = json.dumps(data).encode("utf-8")
+
+        elif self.path == "/status":
+            body = "OK".encode("utf-8")
             self.send_response(200)
-            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
             self.end_headers()
             self.wfile.write(body)
+
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             body = json.dumps(data).encode("utf-8")
@@ -33,12 +31,18 @@ class APIhandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "application/json; charset=utf-8")
             self.end_headers()
             self.wfile.write(body)
-        elif self.path == "/status":
-            body = "OK".encode("utf-8")
+
+        elif self.path == "/info":
+            data = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            body = json.dumps(data).encode("utf-8")
             self.send_response(200)
-            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.send_header("Content-Type", "application/json; charset=utf-8")
             self.end_headers()
             self.wfile.write(body)
+
         else:
             body = "Endpoint not found".encode("utf-8")
             self.send_response(404)
