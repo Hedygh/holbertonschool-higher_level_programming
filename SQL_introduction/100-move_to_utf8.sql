b3c10f6468440cafd15d4611afd638662e23de01
Hedygh <dyouahes@gmail.com>
2026-03-04 21:13:31 +0100

    100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,8 @@
 -- 17. Go to UTF8
--- converts hbtn_0c_0, first_table and name field to utf8mb4 (unicode collation)
+-- converts hbtn_0c_0 database, first_table and name field to utf8mb4 unicode collation
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
